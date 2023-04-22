17ddbc6518a3ea457e43c4e91f5f0004e34deee9
harshittiwarihrt <harshittiwarirt@gmail.com>
2023-04-22 11:11:50 +0530

    testimony constant

--- src/assets/constants/testimonyCheck.js
+++ src/assets/constants/testimonyCheck.js
@@ -64,3 +64,83 @@ export const blabla = [
     },
 
 ]
+
+
+
+export const servicesPage =[
+    {   id:'material-managment',
+        name:'Material Managment',
+        mainPara:'We believe in designing products that are intuitive and user-friendly. Our design team works closely with clients to understand their end-users needs, behaviors, and motivations to create products that deliver an excellent user experience.',
+        img: '/images/imageName.jpg',
+
+        content : [
+            {   sid:1,
+                title:'Title 1',
+                li:[
+                    {liData:'Point1'},
+                    {liData:'Point2'},
+                    {liData:'Point3'},
+                    {liData:'Point4'},
+                ],
+            },
+
+            {   sid:2,
+                title:'Title 2',
+                li:[
+                    {liData:'Point1'},
+                    {liData:'Point2'},
+                    {liData:'Point3'},
+                    {liData:'Point4'},
+                ],
+            },
+
+            {   sid:3,
+                title:'Title 3',
+                li:[
+                    {liData:'Point1'},
+                    {liData:'Point2'},
+                    {liData:'Point3'},
+                    {liData:'Point4'},
+                ],
+            },
+        ],
+    },
+
+    {   id:'finance-and-accounts',
+    name:'Finance and Accounts',
+    mainPara:'We believe in designing products that are intuitive and user-friendly. Our design team works closely with clients to understand their end-users needs, behaviors, and motivations to create products that deliver an excellent user experience.',
+    img: '/images/imageName.jpg',
+
+    content : [
+        {   sid:1,
+            title:'Title 1',
+            li:[
+                {liData:'Point1'},
+                {liData:'Point2'},
+                {liData:'Point3'},
+                {liData:'Point4'},
+            ],
+        },
+
+        {   sid:2,
+            title:'Title 2',
+            li:[
+                {liData:'Point1'},
+                {liData:'Point2'},
+                {liData:'Point3'},
+                {liData:'Point4'},
+            ],
+        },
+
+        {   sid:3,
+            title:'Title 3',
+            li:[
+                {liData:'Point1'},
+                {liData:'Point2'},
+                {liData:'Point3'},
+                {liData:'Point4'},
+            ],
+        },
+    ],
+    },
+]
